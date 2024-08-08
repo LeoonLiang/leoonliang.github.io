564d4bbc9e6d356f6b9a5aa842bad76823942dc9
Leoonliang <dsleoon@gmail.com>
2024-08-08 10:10:07 +0800

    test api

---
+++ api/unsplash.js
@@ -0,0 +1,24 @@
+// githubAuth.js
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+module.exports = async (req, res) => {
+  // 设置请求头允许跨域
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const url = 'https://unsplash.com/napi/users/leoonliang/photos?order_by=latest'
+
+  try{
+    await fetch(url, {
+      method: 'Get',
+    }).then(response => {
+      return response.json();
+
+    }).then(data => {
+      res.json({
+        data
+      });
+    })
+  } catch (error) {}
+}
+
